80981757f4cee4ceaf97b497bbedd6640728fc31
shinya <shinya-works-w@gmail.com>
2021-12-22 13:07:01 +0000

    DBにUserカラムが追加、変更されたとき:emailを小文字かするコールバックメソッドを定義実

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  
   validates :name,  presence: true, length: { maximum: 50 }
+  # :emailの有効条件の定数を定義
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 100 },
+                    # 定義した条件で有効性を検証
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
 end
--- db/migrate/20211222124433_add_index_to_users_email.rb
+++ db/migrate/20211222124433_add_index_to_users_email.rb
@@ -1,4 +1,7 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[5.1]
+  # Usersモデルのデータベースに、indexを追加し、:emailの一意性を検証するクラス
   def change
+    # add_index:rails メソッドの一つ、:unique - trueを指定するとユニークなインデックス
+    add_index :users, :email, unique: true  
   end
 end
