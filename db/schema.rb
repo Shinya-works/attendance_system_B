c16aa4e48d08fb6140d8fe16bb078b0a72d8caeb
shinya <shinya-works-w@gmail.com>
2021-12-22 12:58:48 +0000

    DBにおけるemailの一意性検証実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211222034951) do
+ActiveRecord::Schema.define(version: 20211222124433) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
