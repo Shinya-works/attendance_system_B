603da8e149fdc1e4d2a32483703d48256eac19c4
shinya <shinya-works-w@gmail.com>
2021-12-25 03:09:04 +0000

    ログインユーザーかどうかの認可実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+before_action :logged_in_user, only: [:show, :edit, :update, :destroy]  
   
   def new
     @user = User.new
@@ -33,5 +34,15 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    # beforeフィルター
+    
+    # ログイン済みのユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
     end
 end
